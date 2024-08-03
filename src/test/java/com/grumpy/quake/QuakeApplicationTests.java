ad1203412b638c72fe9679bc7d885d4d819210dc
siddgage <31310947+siddgage@users.noreply.github.com>
2024-08-03 19:43:04 +0530

    initial push

---
+++ src/test/java/com/grumpy/quake/QuakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grumpy.quake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
